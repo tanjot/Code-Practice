9ef379730df6dd4dd10c9b72dc04a831688630ab
Tanjot Kaur <tanjot28@gmail.com>
2018-02-22 08:32:33 +0530

    Arrays::Left Rotation

---
+++ array_leftRotation.cpp
@@ -0,0 +1,99 @@
+//******PROBLEM STATEMENT - Arrays: Left Rotation******/
+//A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
+//
+//Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+//
+//Input Format
+//
+//The first line contains two space-separated integers denoting the respective values of  (the number of integers) and  (the number of left rotations you must perform).
+//The second line contains  space-separated integers describing the respective elements of the array's initial state.
+//
+//Constraints
+//
+//Output Format
+//
+//Print a single line of  space-separated integers denoting the final state of the array after performing  left rotations.
+//
+//Sample Input
+//
+//5 4
+//1 2 3 4 5
+//Sample Output
+//
+//5 1 2 3 4
+//Explanation
+//
+//When we perform  left rotations, the array undergoes the following sequence of changes:
+//
+//Thus, we print the array's final state as a single line of space-separated values, which is 5 1 2 3 4.A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
+//
+//Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+//
+//Input Format
+//
+//The first line contains two space-separated integers denoting the respective values of  (the number of integers) and  (the number of left rotations you must perform).
+//The second line contains  space-separated integers describing the respective elements of the array's initial state.
+//
+//Constraints
+//
+//Output Format
+//
+//Print a single line of  space-separated integers denoting the final state of the array after performing  left rotations.
+//
+//Sample Input
+//
+//5 4
+//1 2 3 4 5
+//Sample Output
+//
+//5 1 2 3 4
+//Explanation
+//
+//When we perform  left rotations, the array undergoes the following sequence of changes:
+//
+//Thus, we print the array's final state as a single line of space-separated values, which is 5 1 2 3 4.
+
+
+//******CODE******/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+
+    if(k < n)
+    {
+        vector<int> tempArr;
+        tempArr.insert(tempArr.begin(),
+                a.begin()+k,
+                a.end());
+        for(int ii=0; ii<(k); ii++) {
+            tempArr.push_back(a[ii]);
+            cout<<a[ii]<<endl;
+        }
+        return tempArr;
+    }
+    else
+    {
+        return a;
+    }
+
+}
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin>> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+    cout<< output[i] <<" ";
+    cout<< endl;
+    return 0;
+}
+
+
